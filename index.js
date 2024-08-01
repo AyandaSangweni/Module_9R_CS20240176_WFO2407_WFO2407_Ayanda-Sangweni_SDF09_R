555cef909c887beb00a3e27a51d42bdf3275c3c3
Ayanda Sangweni <asangweni51@gmail.com>
2024-08-01 22:07:42 +0200

    add diplay the cards

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
-console.log(messageEl)
 let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
 
 function startGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card? "
